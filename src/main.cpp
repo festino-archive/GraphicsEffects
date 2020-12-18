538dec71cf9c1029aaa50a0fc593c3607b6e9543
Festino <offestashka@mail.ru>
2020-12-18 15:50:37 +0300

    Release v1.0

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "scene_1.h"
-//#include "Examples/NormalMapping.h"
-
-int
-main(int argc, char** argv)
-{
-    //return ex5::start_example(argc, argv);
-    return start_scene_1(argc, argv);
-}
--- src/scene_1.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-int start_scene_1(int argc, char** argv);
